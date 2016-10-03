28129989d718391ceb7204a817dfcde89f5414e7
vidya <vipaygud@microsoft.com>
2016-10-03 15:13:39 -0700

    Added console writeline

--- TestGitPeoject/Program.cs
+++ TestGitPeoject/Program.cs
@@ -11,6 +11,10 @@ namespace TestGitPeoject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+
         }
     }
 }
